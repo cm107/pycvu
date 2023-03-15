a4d72b62e9b697637f92d36a03c23eb464314532
Clayton Mork <mork.clayton3@gmail.com>
2023-03-15 16:50:19 +0900

    Fix error when installing from github url.

--- setup.py
+++ setup.py
@@ -24,60 +24,60 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
-class SetupSpec:
-    def __init__(self, required: list[str]):
-        self.required = required
-        self.extras: dict[str, list[str]] = {}
-        self.entry_points: dict[str, list[str]] = {}
+# class SetupSpec:
+#     def __init__(self, required: list[str]):
+#         self.required = required
+#         self.extras: dict[str, list[str]] = {}
+#         self.entry_points: dict[str, list[str]] = {}
     
-    def add_console_scripts(self, value: str | list[str], extra_tags: str | list[str]=None):
-        if 'console_scripts' not in self.entry_points:
-            self.entry_points['console_scripts'] = []
-        if type(value) is str:
-            value = [value]
-        if extra_tags is not None:
-            if type(extra_tags) is str:
-                extra_tags = [extra_tags]
-            for i in range(len(value)):
-                value[i] = f"{value[i]} [{','.join(extra_tags)}]"
-        self.entry_points['console_scripts'].extend(value)
-        print(f"{self.entry_points['console_scripts']=}")
+#     def add_console_scripts(self, value: str | list[str], extra_tags: str | list[str]=None):
+#         if 'console_scripts' not in self.entry_points:
+#             self.entry_points['console_scripts'] = []
+#         if type(value) is str:
+#             value = [value]
+#         if extra_tags is not None:
+#             if type(extra_tags) is str:
+#                 extra_tags = [extra_tags]
+#             for i in range(len(value)):
+#                 value[i] = f"{value[i]} [{','.join(extra_tags)}]"
+#         self.entry_points['console_scripts'].extend(value)
+#         print(f"{self.entry_points['console_scripts']=}")
 
-setupSpec = SetupSpec(
-    [
-        'pylint>=2.15.5',
-        'numpy>=1.23.4',
-        'opencv-python>=4.6.0.66',
-        'Pillow>=9.2.0',
-        'PyYAML>=6.0',
-        'joblib>=1.2.0',
-        'tqdm>=4.64.1',
-        'pyevu @ git+ssh://git@github.com/cm107/pyevu.git@master'
-    ]
-)
-setupSpec.extras['pdf'] = [
-    'PyMuPDF==1.19.6'
-]
-setupSpec.extras['detectron2'] = [
-    # TODO
-]
-setupSpec.extras['nlp'] = [
-    'transformers>=4.25.1'
-]
-setupSpec.extras['all'] = list(set(
-    chain.from_iterable(setupSpec.extras.values())
-))
+# setupSpec = SetupSpec(
+#     [
+#         'pylint>=2.15.5',
+#         'numpy>=1.23.4',
+#         'opencv-python>=4.6.0.66',
+#         'Pillow>=9.2.0',
+#         'PyYAML>=6.0',
+#         'joblib>=1.2.0',
+#         'tqdm>=4.64.1',
+#         'pyevu @ git+ssh://git@github.com/cm107/pyevu.git@master'
+#     ]
+# )
+# setupSpec.extras['pdf'] = [
+#     'PyMuPDF==1.19.6'
+# ]
+# setupSpec.extras['detectron2'] = [
+#     # TODO
+# ]
+# setupSpec.extras['nlp'] = [
+#     'transformers>=4.25.1'
+# ]
+# setupSpec.extras['all'] = list(set(
+#     chain.from_iterable(setupSpec.extras.values())
+# ))
 
-setupSpec.add_console_scripts(
-    [
-        "pycvu-coco-show_preview=pycvu.coco.cli.show_preview",
-        "pycvu-artist-generate_dataset=pycvu.artist.cli.generate_dataset",
-    ]
-)
-setupSpec.add_console_scripts(
-    "pycvu-pdf_to_png=pycvu.util.pdf.cli.pdf_to_png",
-    extra_tags=['pdf', 'all']
-)
+# setupSpec.add_console_scripts(
+#     [
+#         "pycvu-coco-show_preview=pycvu.coco.cli.show_preview",
+#         "pycvu-artist-generate_dataset=pycvu.artist.cli.generate_dataset",
+#     ]
+# )
+# setupSpec.add_console_scripts(
+#     "pycvu-pdf_to_png=pycvu.util.pdf.cli.pdf_to_png",
+#     extra_tags=['pdf', 'all']
+# )
 
 setup(
     name='pycvu',
@@ -92,8 +92,22 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=setupSpec.required,
-    extras_require=setupSpec.extras,
+    install_requires=[
+        'pylint>=2.15.5',
+        'numpy>=1.23.4',
+        'opencv-python>=4.6.0.66',
+        'Pillow>=9.2.0',
+        'PyYAML>=6.0',
+        'joblib>=1.2.0',
+        'tqdm>=4.64.1',
+        'pyevu @ git+ssh://git@github.com/cm107/pyevu.git@master',
+        'PyMuPDF>=1.19.6'
+    ],
     python_requires='>=3.10',
-    entry_points=setupSpec.entry_points
+    entry_points={
+        'console_scripts': [
+            "pycvu-coco-show_preview=pycvu.coco.cli.show_preview",
+            "pycvu-artist-generate_dataset=pycvu.artist.cli.generate_dataset",
+        ]
+    }
 )
